3d1c7e71dfc498f2cfd18cccd877c9cf25463b51
angstsmurf <ignalina@me.com>
2020-12-29 13:09:28 +0100

    Initial commit of Spotlight and Quick Look

---
+++ SpatterlightMetadataImporter/IndexRequestHandler.h
@@ -0,0 +1,12 @@
+//
+//  IndexRequestHandler.h
+//  SpatterlightMetadataImporter
+//
+//  Created by Administrator on 2020-12-29.
+//
+
+#import <CoreSpotlight/CoreSpotlight.h>
+
+@interface IndexRequestHandler : CSIndexExtensionRequestHandler
+
+@end
---
+++ SpatterlightQuickLook/ThumbnailProvider.h
@@ -0,0 +1,16 @@
+//
+//  ThumbnailProvider.h
+//  SpatterlightQuickLook
+//
+//  Created by Administrator on 2020-12-29.
+//
+
+#import <QuickLookThumbnailing/QuickLookThumbnailing.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ThumbnailProvider : QLThumbnailProvider
+
+@end
+
+NS_ASSUME_NONNULL_END
